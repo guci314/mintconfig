af87482a8c16c366da6f825e1d2e586212030eda
guci <guci314@gmail.com>
2016-03-17 05:53:53 +0800

    test

---
+++ push-mintconfig.sh
@@ -0,0 +1,9 @@
+cp /home/guci/XX-Net-2.5.5/data/gae_proxy/good_ip.txt ./mintconfig
+cd mintconfig
+git add .
+git commit -m "test"
+rm ~/.git-credentials
+touch ~/.git-credentials
+echo "https://guci314:guci72101137748@github.com" >  ~/.git-credentials
+git config --global credential.helper store
+git push origin
